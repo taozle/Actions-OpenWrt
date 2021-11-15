7f2214de0ee57773abae9fb2b75c9f720bf44d7e
taozle <zhangliyang26@gmail.com>
2021-11-15 14:16:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone --depth=1 https://github.com/rosywrt/luci-theme-rosy -b openwrt-18.06 package/rosywrt/luci-theme-rosy
